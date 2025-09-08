614916deb7e621962f456e56a2eb79f1a316d2cf
dorodrig <rodriguezdavid386@gmail.com>
2025-09-08 18:25:54 -0500

    FORCE DEPLOY: Actualización para GitHub Pages - 2025-09-08-18:25

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.scss'
 import App from './App.tsx'
 import './config/chartConfig'
 
+// Production Update - Build: 2025-09-08-23:25
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

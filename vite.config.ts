eaf3e573822c16e81ee522ffbfda852e0ba3b0c8
dorodrig <rodriguezdavid386@gmail.com>
2025-10-29 21:34:47 -0500

    correcion archivo vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 // import { VitePWA } from 'vite-plugin-pwa'; // Deshabilitado temporalmente por error crypto.hash
 
 // https://vitejs.dev/config/
@@ -43,6 +44,14 @@ export default defineConfig({
     //   manifest: false, // Usar manifest.json estático de public/
     // })
   ],
+    // 🏗️ Alias modernos para rutas en React y SCSS
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '/assets': path.resolve(__dirname, './src/assets'),
+      '/styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   base: '/', // EC2 Single Server - servir desde raíz
   server: {
     host: 'localhost',
@@ -64,10 +73,14 @@ export default defineConfig({
     },
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
+      outDir: 'dist',
+      sourcemap: false,
+      emptyOutDir: true,
+      cssCodeSplit: true,
+      rollupOptions: {
+          output: {
+            manualChunks: undefined,
+          },
       },
-    },
   },
 });
